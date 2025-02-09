36481c81e9349dd8d39cb25f71f2135c64b78475
Bjarke Falch <bs@wasd.dk>
2025-02-09 22:53:29 +0100

    make services the default app (@ /)

--- keongom/keongom/urls.py
+++ keongom/keongom/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("services/", include("services.urls")),
+    path("", include("services.urls")),  # ## services is the default app
 ]
